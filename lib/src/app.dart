3ceca00c13960df96a84c1d9e686be6680bd88a2
Adam Fronia <adamfronia44@gmail.com>
2024-05-02 15:34:06 +0200

    splashscreen,theme.schriftarten

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,5 +1,6 @@
 import 'package:flex_color_scheme/flex_color_scheme.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:ohanap/src/features/welcome/presentation/splash_screen.dart';
 
 class App extends StatelessWidget {
@@ -9,7 +10,11 @@ class App extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: const SplashScreen(),
-      theme: FlexThemeData.light(scheme: FlexScheme.aquaBlue),
+      theme: FlexThemeData.light(scheme: FlexScheme.aquaBlue).copyWith(
+        textTheme: TextTheme(
+          bodyMedium: GoogleFonts.akayaKanadaka(),
+        ),
+      ),
       darkTheme: FlexThemeData.dark(scheme: FlexScheme.mandyRed),
       themeMode: ThemeMode.system,
     );
--- lib/src/features/welcome/presentation/splash_screen.dart
+++ lib/src/features/welcome/presentation/splash_screen.dart
@@ -6,38 +6,47 @@ class SplashScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        width: double.infinity,
-        height: double.infinity,
-        padding: const EdgeInsets.only(bottom: 548),
-        decoration: const BoxDecoration(
-          gradient: LinearGradient(
-            begin: Alignment.topCenter,
-            end: Alignment.bottomCenter,
-            colors: [
-              Color(0xFF0EECFA),
-              Color(0x821697D7),
-            ],
-          ),
-        ),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Image.network(
-              'assets/Bildschirmfoto 2024-05-02 um 14.03.38.png',
-              width: 390,
-              height: 296,
-            ),
-            const SizedBox(height: 20),
-            const Text(
-              'Willkommen',
-              style: TextStyle(
-                fontSize: 24,
-                fontWeight: FontWeight.bold,
+      body: Stack(
+        children: [
+          Container(
+            width: double.infinity,
+            height: double.infinity,
+            padding: const EdgeInsets.only(bottom: 508),
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage(
+                  'assets/Bildschirmfoto.png',
+                ),
+                fit: BoxFit.cover,
+              ),
+              gradient: LinearGradient(
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter,
+                colors: [
+                  Color(0xFF0EECFA),
+                  Color(0x821697D7),
+                ],
               ),
             ),
-          ],
-        ),
+          ),
+          const Positioned(
+            top: 300,
+            left: 0,
+            right: 0,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Text(
+                  'Willkommen',
+                  // style: TextStyle(
+                  //   fontSize: 24,
+                  //   fontWeight: FontWeight.bold,
+                  // ),
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
