ed4a7da5f73d667fe8dafa4db671198b239ba7d7
AdamFronia <166802660+AdamFronia@users.noreply.github.com>
2024-06-26 12:32:53 +0200

    ohana firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:ohanap/firebase_options.dart';
 import 'package:ohanap/src/app.dart';
 
 Future<void> main() async {
