26c95283e00595ff26a05328c68584152bfd6020
AdamFronia <166802660+AdamFronia@users.noreply.github.com>
2024-07-27 10:25:34 +0200

    Image Picker setzten noch nicht fertig mit firebase verbunden

--- lib/src/features/friendbook/domain/profile.dart
+++ lib/src/features/friendbook/domain/profile.dart
@@ -27,41 +27,44 @@ class Profile {
   String? thatsMyStrengths;
   String? funnyFact;
   String? futuretime;
+  String? mainProfileURL;
+  String? secondImageProfileURL;
 
   // Document ID
   final String docID;
 
   // Konstruktor
-  Profile({
-    required this.futures,
-    required this.funnys,
-    required this.goodies,
-    required this.profilePicUrl,
-    required this.name,
-    required this.readme,
-    required this.relationShip,
-    required this.city,
-    required this.hobby,
-    required this.holiday,
-    required this.job,
-    required this.wishJob,
-    required this.color,
-    required this.birthdate,
-    required this.sleepTime,
-    required this.hasSiblings,
-    required this.likeSports,
-    required this.likesReading,
-    required this.aboutMe,
-    required this.dataList,
-    required this.animal,
-    required this.drink,
-    required this.musik,
-    required this.essen,
-    required this.docID,
-    this.thatsMyStrengths,
-    this.funnyFact,
-    this.futuretime,
-  });
+  Profile(
+      {required this.futures,
+      required this.funnys,
+      required this.goodies,
+      required this.profilePicUrl,
+      required this.name,
+      required this.readme,
+      required this.relationShip,
+      required this.city,
+      required this.hobby,
+      required this.holiday,
+      required this.job,
+      required this.wishJob,
+      required this.color,
+      required this.birthdate,
+      required this.sleepTime,
+      required this.hasSiblings,
+      required this.likeSports,
+      required this.likesReading,
+      required this.aboutMe,
+      required this.dataList,
+      required this.animal,
+      required this.drink,
+      required this.musik,
+      required this.essen,
+      required this.docID,
+      this.thatsMyStrengths,
+      this.funnyFact,
+      this.futuretime,
+      this.mainProfileURL,
+      this.secondImageProfileURL});
 
 //Methodes
 
@@ -94,39 +97,42 @@ class Profile {
       "thatsMyStrengths": thatsMyStrengths,
       "funnyFact": funnyFact,
       "futuretime": futuretime,
+      "mainProfileURL": mainProfileURL,
+      "secondImageProfileURL": secondImageProfileURL
     };
   }
 
   factory Profile.fromMap(Map<String, dynamic> map, String docID) {
     return Profile(
-      docID: docID,
-      futures: map['futures'],
-      funnys: map['funnys'],
-      goodies: map['goodies'],
-      essen: map['essen'],
-      drink: map['Getraenke'],
-      musik: map['musik'],
-      animal: map['tier'],
-      profilePicUrl: map['profilePicUrl'],
-      name: map['name'],
-      readme: map['readme'],
-      relationShip: map['relationShip'],
-      city: map['city'],
-      hobby: map['hobby'],
-      holiday: map['holiday'],
-      job: map['job'],
-      wishJob: map['wishJob'],
-      color: map['color'], // Color von int wiederherstellen
-      birthdate: map['birthdate'],
-      sleepTime: map['sleepTime'],
-      hasSiblings: map['hasSiblings'],
-      likeSports: map['likeSports'],
-      likesReading: map['likesReading'],
-      aboutMe: map['aboutMe'],
-      dataList: List<Map<String, dynamic>>.from(map['dataList']),
-      thatsMyStrengths: map['thatsMyStrengths'],
-      funnyFact: map['funnyFact'],
-      futuretime: map['futuretime'],
-    );
+        docID: docID,
+        futures: map['futures'],
+        funnys: map['funnys'],
+        goodies: map['goodies'],
+        essen: map['essen'],
+        drink: map['Getraenke'],
+        musik: map['musik'],
+        animal: map['tier'],
+        profilePicUrl: map['profilePicUrl'],
+        name: map['name'],
+        readme: map['readme'],
+        relationShip: map['relationShip'],
+        city: map['city'],
+        hobby: map['hobby'],
+        holiday: map['holiday'],
+        job: map['job'],
+        wishJob: map['wishJob'],
+        color: map['color'], // Color von int wiederherstellen
+        birthdate: map['birthdate'],
+        sleepTime: map['sleepTime'],
+        hasSiblings: map['hasSiblings'],
+        likeSports: map['likeSports'],
+        likesReading: map['likesReading'],
+        aboutMe: map['aboutMe'],
+        dataList: List<Map<String, dynamic>>.from(map['dataList']),
+        thatsMyStrengths: map['thatsMyStrengths'],
+        funnyFact: map['funnyFact'],
+        futuretime: map['futuretime'],
+        mainProfileURL: map["mainProfileURL"],
+        secondImageProfileURL: map["secondImageProfileURL"]);
   }
 }
--- lib/src/features/home/presentation/profile/profile_picture.dart
+++ lib/src/features/home/presentation/profile/profile_picture.dart
@@ -1,9 +1,43 @@
-import 'package:flutter/material.dart';
+import 'dart:io';
 
-class ProfilePicture extends StatelessWidget {
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:image_picker/image_picker.dart';
+
+class ProfilePicture extends StatefulWidget {
   const ProfilePicture({
     super.key,
   });
+
+  @override
+  State<ProfilePicture> createState() => _ProfilePictureState();
+}
+
+class _ProfilePictureState extends State<ProfilePicture> {
+  File? image;
+  File? image2;
+
+  Future pickImage(ImageSource source) async {
+    try {
+      final image = await ImagePicker().pickImage(source: source);
+      if (image == null) return;
+      final imageTemp = File(image.path);
+      setState(() => this.image = imageTemp);
+    } on PlatformException catch (e) {
+      print('Failed to pick image: $e');
+    }
+  }
+
+  Future pickImage2(ImageSource source) async {
+    try {
+      final image2 = await ImagePicker().pickImage(source: source);
+      if (image2 == null) return;
+      final image2Temp = File(image2.path);
+      setState(() => this.image2 = image2Temp);
+    } on PlatformException catch (e) {
+      print('Failed to pick image: $e');
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -11,19 +45,105 @@ class ProfilePicture extends StatelessWidget {
       children: [
         Align(
           alignment: Alignment.topLeft + const Alignment(1.0, 3.2),
-          child: Image.asset(
-            'assets/ostseebroetchen.png',
-            width: 150,
-            height: 150,
+          child: GestureDetector(
+            onTap: () {
+              showModalBottomSheet(
+                context: context,
+                builder: (context) => BottomSheet(
+                  onClosing: () {},
+                  builder: (context) => Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      ListTile(
+                        leading: const Icon(Icons.camera),
+                        title: const Text('Camera'),
+                        onTap: () {
+                          pickImage(ImageSource.camera);
+                          Navigator.of(context).pop();
+                        },
+                      ),
+                      ListTile(
+                        leading: const Icon(Icons.photo_library),
+                        title: const Text('Gallery'),
+                        onTap: () {
+                          pickImage(ImageSource.gallery);
+                          Navigator.of(context).pop();
+                        },
+                      ),
+                    ],
+                  ),
+                ),
+              );
+            },
+            child: image != null
+                ? Image.file(
+                    fit: BoxFit.cover,
+                    image!,
+                    width: 150,
+                    height: 150,
+                  )
+                : Container(
+                    width: 150,
+                    height: 150,
+                    color: Colors.grey, // Placeholder color or image
+                    child: const Icon(
+                      Icons.person_add_outlined,
+                      size: 55,
+                      color: Colors.white,
+                    ),
+                  ),
           ),
         ),
         Positioned(
           right: 80,
           bottom: 0,
-          child: Image.asset(
-            'assets/papashund.png',
-            width: 75,
-            height: 75,
+          child: GestureDetector(
+            onTap: () {
+              showModalBottomSheet(
+                context: context,
+                builder: (context) => BottomSheet(
+                  onClosing: () {},
+                  builder: (context) => Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      ListTile(
+                        leading: const Icon(Icons.camera),
+                        title: const Text('Camera'),
+                        onTap: () {
+                          pickImage2(ImageSource.camera);
+                          Navigator.of(context).pop();
+                        },
+                      ),
+                      ListTile(
+                        leading: const Icon(Icons.photo_library),
+                        title: const Text('Gallery'),
+                        onTap: () {
+                          pickImage2(ImageSource.gallery);
+                          Navigator.of(context).pop();
+                        },
+                      ),
+                    ],
+                  ),
+                ),
+              );
+            },
+            child: image2 != null
+                ? Image.file(
+                    fit: BoxFit.cover,
+                    image2!,
+                    width: 75,
+                    height: 75,
+                  )
+                : Container(
+                    width: 75,
+                    height: 75,
+                    color: Colors.grey, // Placeholder color or image
+                    child: const Icon(
+                      Icons.person_add_outlined,
+                      size: 45,
+                      color: Colors.white,
+                    ),
+                  ),
           ),
         ),
       ],
